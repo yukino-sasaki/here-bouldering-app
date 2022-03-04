adfdf3ca9d308e86aea4cd17634442b3ba0d2319
yukino-sasaki <yukino@oisu.jp>
2022-03-04 18:01:02 +0900

    feat: make home UI and some components

---
+++ client/src/dummy.js
@@ -0,0 +1,22 @@
+export const gym = [
+  {
+    name: "1st Wally",
+    place: "藤沢市",
+  },
+  {
+    name: "1st Wally",
+    place: "藤沢市",
+  },
+  {
+    name: "1st Wally",
+    place: "藤沢市",
+  },
+  {
+    name: "1st Wally",
+    place: "藤沢市",
+  },
+  {
+    name: "1st Wally",
+    place: "藤沢市",
+  },
+];
--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import {
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+import "focus-visible/dist/focus-visible";
+import { useNavigate } from "react-router-dom";
+import { Modal } from "../components/Modal";
+import { gym } from "../dummy";
 
 const HomeScreen = () => {
+  const navigate = useNavigate();
+
+  const createGym = useDisclosure();
+  const userSetting = useDisclosure();
+
+  const navigatePage = () => {
+    // wish to navigate each id. it's tentative
+    navigate("/detail");
+  };
+
   return (
     <>
       <div className="bg-darkgray flex-col justify-center h-16 flex">
@@ -8,13 +31,63 @@ const HomeScreen = () => {
       </div>
       <div className="flex h-full">
         <div className="bg-gray w-1/5 h-screen">
-          <h3 className="text-white">ユーザー名変更</h3>
-          <h3 className="text-white">ジムを登録</h3>
+          <Button colorScheme="white" isFullWidth onClick={userSetting.onOpen}>
+            ユーザー名を変更する
+          </Button>
+          <Button colorScheme="white" isFullWidth onClick={createGym.onOpen}>
+            新規でジムを作成する
+          </Button>
         </div>
-        <div className="bg-white w-4/5">
-          <div className="text-3xl text-red-500 text-center">home</div>
+        <div className="bg-white w-4/5 px-5 pt-8">
+          <Grid gap={5} templateColumns="repeat(4, 1fr)">
+            {gym.map(({ name, place }, i) => {
+              return (
+                <GridItem
+                  w="100%"
+                  h="28"
+                  bg="white"
+                  borderRadius="md"
+                  boxShadow="md"
+                  onClick={navigatePage}
+                >
+                  <Center w="100%" bg="blue.500" h="20" borderTopRadius="md">
+                    <div className="text-white text-2xl">{name}</div>
+                  </Center>
+                  <div className="px-3 leading-8">{place}</div>
+                </GridItem>
+              );
+            })}
+          </Grid>
         </div>
       </div>
+
+      <Modal
+        isOpen={createGym.isOpen}
+        onClose={createGym.onClose}
+        header="ジムを新規作成する"
+        submit="作成する"
+      >
+        <FormControl>
+          <FormLabel>ジムの名前</FormLabel>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormLabel>ジムの場所</FormLabel>
+          <Input />
+        </FormControl>
+      </Modal>
+
+      <Modal
+        isOpen={userSetting.isOpen}
+        onClose={userSetting.onClose}
+        header={"ユーザー名を変更する"}
+        submit="変更する"
+      >
+        <FormControl>
+          <FormLabel>変更後のユーザー名</FormLabel>
+          <Input />
+        </FormControl>
+      </Modal>
     </>
   );
 };
