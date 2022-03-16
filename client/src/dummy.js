b88bc92acff899762ac06b3a3a5a35bc1d3ffb61
yukino-sasaki <yukino@oisu.jp>
2022-03-16 17:43:09 +0900

    feat: add home detail

--- client/src/dummy.js
+++ client/src/dummy.js
@@ -20,3 +20,31 @@ export const gym = [
     place: "藤沢市",
   },
 ];
+
+export const gymInformation = {
+  name: "1st Wally",
+  creater: "yukis",
+  place: "fujisawa",
+  member: [
+    {
+      memberName: "kazu",
+      comment: "climbing",
+      time: "10:00~22:00",
+    },
+    {
+      memberName: "kazu",
+      comment: "climbing",
+      time: "10:00~22:00",
+    },
+    {
+      memberName: "kazu",
+      comment: "climbing",
+      time: "10:00~22:00",
+    },
+    {
+      memberName: "kazu",
+      comment: "climbing",
+      time: "10:00~22:00",
+    },
+  ],
+};
--- client/src/screens/HereDetailScreen.tsx
+++ client/src/screens/HereDetailScreen.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { Box, Center, Divider } from "@chakra-ui/react";
+import { gymInformation } from "../dummy";
 
 const HereDetailScreen = () => {
-  return <div>detail</div>;
+  const { name, creater, member } = gymInformation;
+  return (
+    <div className="mx-auto">
+      <Center
+        w="100%"
+        h="40"
+        borderWidth="1px"
+        borderRadius="lg"
+        color="white"
+        bg="blue.500"
+        maxW="lg"
+      >
+        {name}
+      </Center>
+      {member.map((member, index) => {
+        return (
+          <>
+            <Box>{member}</Box>
+            <Divider />
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HereDetailScreen;
